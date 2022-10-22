5f488b600745ce659d120e6245d505d629a33564
BreizhHardware <fpmovie4@gmail.com>
2022-10-22 15:14:54 +0200

    Update to the 1.1

---
+++ overrides/kubejs/server_scripts/gardencloche.js
@@ -0,0 +1,150 @@
+onEvent('recipes', event => {
+
+    /*********************************** VANILLA ***********************************/
+    event.remove({ id: 'immersiveengineering:cloche/wheat' })
+    gardenvanilla('minecraft:wheat', 1, 'minecraft:wheat_seeds', 'minecraft:dirt', 'crop', 'minecraft:wheat')
+    gardenvanilla('minecraft:wheat', 2, 'minecraft:wheat_seeds', 'mysticalagriculture:inferium_farmland', 'crop', 'minecraft:wheat')
+    gardenvanilla('minecraft:wheat', 3, 'minecraft:wheat_seeds', 'mysticalagriculture:prudentium_farmland', 'crop', 'minecraft:wheat')
+    gardenvanilla('minecraft:wheat', 4, 'minecraft:wheat_seeds', 'mysticalagriculture:tertium_farmland', 'crop', 'minecraft:wheat')
+    gardenvanilla('minecraft:wheat', 5, 'minecraft:wheat_seeds', 'mysticalagriculture:imperium_farmland', 'crop', 'minecraft:wheat')
+    gardenvanilla('minecraft:wheat', 6, 'minecraft:wheat_seeds', 'mysticalagriculture:supremium_farmland', 'crop', 'minecraft:wheat')
+    gardenvanilla('minecraft:wheat', 7, 'minecraft:wheat_seeds', 'mysticalagradditions:insanium_farmland', 'crop', 'minecraft:wheat')
+    
+    event.remove({ id: 'immersiveengineering:cloche/sweet_berries' })
+    gardenvanilla('minecraft:sweet_berries', 1, 'minecraft:sweet_berries', 'minecraft:dirt', 'crop', 'minecraft:sweet_berry_bush')
+    gardenvanilla('minecraft:sweet_berries', 2, 'minecraft:sweet_berries', 'mysticalagriculture:inferium_farmland', 'crop', 'minecraft:sweet_berry_bush')
+    gardenvanilla('minecraft:sweet_berries', 3, 'minecraft:sweet_berries', 'mysticalagriculture:prudentium_farmland', 'crop', 'minecraft:sweet_berry_bush')
+    gardenvanilla('minecraft:sweet_berries', 4, 'minecraft:sweet_berries', 'mysticalagriculture:tertium_farmland', 'crop', 'minecraft:sweet_berry_bush')
+    gardenvanilla('minecraft:sweet_berries', 5, 'minecraft:sweet_berries', 'mysticalagriculture:imperium_farmland', 'crop', 'minecraft:sweet_berry_bush')
+    gardenvanilla('minecraft:sweet_berries', 6, 'minecraft:sweet_berries', 'mysticalagriculture:supremium_farmland', 'crop', 'minecraft:sweet_berry_bush')
+    gardenvanilla('minecraft:sweet_berries', 7, 'minecraft:sweet_berries', 'mysticalagradditions:insanium_farmland', 'crop', 'minecraft:sweet_berry_bush')
+    
+    event.remove({ id: 'immersiveengineering:cloche/sugar_cane' })
+    gardenvanilla('minecraft:sugar_cane', 1, 'minecraft:sugar_cane', 'minecraft:sand', 'stacking', 'minecraft:sugar_cane')
+    
+    event.remove({ id: 'immersiveengineering:cloche/cactus' })
+    gardenvanilla('minecraft:cactus', 1, 'minecraft:cactus', 'minecraft:sand', 'stacking', 'minecraft:cactus')
+    
+    event.remove({ id: 'immersiveengineering:cloche/bamboo' })
+    gardenvanilla('minecraft:bamboo', 1, 'minecraft:bamboo', 'minecraft:dirt', 'stacking', 'minecraft:bamboo')
+    gardenvanilla('minecraft:bamboo', 2, 'minecraft:bamboo', 'mysticalagriculture:inferium_farmland', 'stacking', 'minecraft:bamboo')
+    gardenvanilla('minecraft:bamboo', 3, 'minecraft:bamboo', 'mysticalagriculture:prudentium_farmland', 'stacking', 'minecraft:bamboo')
+    gardenvanilla('minecraft:bamboo', 4, 'minecraft:bamboo', 'mysticalagriculture:tertium_farmland', 'stacking', 'minecraft:bamboo')
+    gardenvanilla('minecraft:bamboo', 5, 'minecraft:bamboo', 'mysticalagriculture:imperium_farmland', 'stacking', 'minecraft:bamboo')
+    gardenvanilla('minecraft:bamboo', 6, 'minecraft:bamboo', 'mysticalagriculture:supremium_farmland', 'stacking', 'minecraft:bamboo')
+    gardenvanilla('minecraft:bamboo', 7, 'minecraft:bamboo', 'mysticalagradditions:insanium_farmland', 'stacking', 'minecraft:bamboo')
+    
+    event.remove({ id: 'immersiveengineering:cloche/beetroot' })
+    gardenvanilla('minecraft:beetroot', 1, 'minecraft:beetroot_seeds', 'minecraft:dirt', 'crop', 'minecraft:beetroots')
+    gardenvanilla('minecraft:beetroot', 2, 'minecraft:beetroot_seeds', 'mysticalagriculture:inferium_farmland', 'crop', 'minecraft:beetroots')
+    gardenvanilla('minecraft:beetroot', 3, 'minecraft:beetroot_seeds', 'mysticalagriculture:prudentium_farmland', 'crop', 'minecraft:beetroots')
+    gardenvanilla('minecraft:beetroot', 4, 'minecraft:beetroot_seeds', 'mysticalagriculture:tertium_farmland', 'crop', 'minecraft:beetroots')
+    gardenvanilla('minecraft:beetroot', 5, 'minecraft:beetroot_seeds', 'mysticalagriculture:imperium_farmland', 'crop', 'minecraft:beetroots')
+    gardenvanilla('minecraft:beetroot', 6, 'minecraft:beetroot_seeds', 'mysticalagriculture:supremium_farmland', 'crop', 'minecraft:beetroots')
+    gardenvanilla('minecraft:beetroot', 7, 'minecraft:beetroot_seeds', 'mysticalagradditions:insanium_farmland', 'crop', 'minecraft:beetroots')
+    
+    event.remove({ id: 'immersiveengineering:cloche/brown_mushroom' })
+    gardenvanilla('minecraft:brown_mushroom', 1, 'minecraft:brown_mushroom', 'minecraft:dirt', 'generic', 'minecraft:brown_mushroom')
+    gardenvanilla('minecraft:brown_mushroom', 2, 'minecraft:brown_mushroom', 'mysticalagriculture:inferium_farmland', 'generic', 'minecraft:brown_mushroom')
+    gardenvanilla('minecraft:brown_mushroom', 3, 'minecraft:brown_mushroom', 'mysticalagriculture:prudentium_farmland', 'generic', 'minecraft:brown_mushroom')
+    gardenvanilla('minecraft:brown_mushroom', 4, 'minecraft:brown_mushroom', 'mysticalagriculture:tertium_farmland', 'generic', 'minecraft:brown_mushroom')
+    gardenvanilla('minecraft:brown_mushroom', 5, 'minecraft:brown_mushroom', 'mysticalagriculture:imperium_farmland', 'generic', 'minecraft:brown_mushroom')
+    gardenvanilla('minecraft:brown_mushroom', 6, 'minecraft:brown_mushroom', 'mysticalagriculture:supremium_farmland', 'generic', 'minecraft:brown_mushroom')
+    gardenvanilla('minecraft:brown_mushroom', 7, 'minecraft:brown_mushroom', 'mysticalagradditions:insanium_farmland', 'generic', 'minecraft:brown_mushroom')
+    
+    event.remove({ id: 'immersiveengineering:cloche/red_mushroom' })
+    gardenvanilla('minecraft:red_mushroom', 1, 'minecraft:red_mushroom', 'minecraft:dirt', 'generic', 'minecraft:red_mushroom')
+    gardenvanilla('minecraft:red_mushroom', 2, 'minecraft:red_mushroom', 'mysticalagriculture:inferium_farmland', 'generic', 'minecraft:red_mushroom')
+    gardenvanilla('minecraft:red_mushroom', 3, 'minecraft:red_mushroom', 'mysticalagriculture:prudentium_farmland', 'generic', 'minecraft:red_mushroom')
+    gardenvanilla('minecraft:red_mushroom', 4, 'minecraft:red_mushroom', 'mysticalagriculture:tertium_farmland', 'generic', 'minecraft:red_mushroom')
+    gardenvanilla('minecraft:red_mushroom', 5, 'minecraft:red_mushroom', 'mysticalagriculture:imperium_farmland', 'generic', 'minecraft:red_mushroom')
+    gardenvanilla('minecraft:red_mushroom', 6, 'minecraft:red_mushroom', 'mysticalagriculture:supremium_farmland', 'generic', 'minecraft:red_mushroom')
+    gardenvanilla('minecraft:red_mushroom', 7, 'minecraft:red_mushroom', 'mysticalagradditions:insanium_farmland', 'generic', 'minecraft:red_mushroom')
+    
+    event.remove({ id: 'immersiveengineering:cloche/carrot' })
+    gardenvanilla('minecraft:carrot', 1, 'minecraft:carrot', 'minecraft:dirt', 'crop', 'minecraft:carrots')
+    gardenvanilla('minecraft:carrot', 2, 'minecraft:carrot', 'mysticalagriculture:inferium_farmland', 'crop', 'minecraft:carrots')
+    gardenvanilla('minecraft:carrot', 3, 'minecraft:carrot', 'mysticalagriculture:prudentium_farmland', 'crop', 'minecraft:carrots')
+    gardenvanilla('minecraft:carrot', 4, 'minecraft:carrot', 'mysticalagriculture:tertium_farmland', 'crop', 'minecraft:carrots')
+    gardenvanilla('minecraft:carrot', 5, 'minecraft:carrot', 'mysticalagriculture:imperium_farmland', 'crop', 'minecraft:carrots')
+    gardenvanilla('minecraft:carrot', 6, 'minecraft:carrot', 'mysticalagriculture:supremium_farmland', 'crop', 'minecraft:carrots')
+    gardenvanilla('minecraft:carrot', 7, 'minecraft:carrot', 'mysticalagradditions:insanium_farmland', 'crop', 'minecraft:carrots')
+    
+    event.remove({ id: 'immersiveengineering:cloche/potato' })
+    gardenvanilla('minecraft:potato', 1, 'minecraft:potato', 'minecraft:dirt', 'crop', 'minecraft:potatoes')
+    gardenvanilla('minecraft:potato', 2, 'minecraft:potato', 'mysticalagriculture:inferium_farmland', 'crop', 'minecraft:potatoes')
+    gardenvanilla('minecraft:potato', 3, 'minecraft:potato', 'mysticalagriculture:prudentium_farmland', 'crop', 'minecraft:potatoes')
+    gardenvanilla('minecraft:potato', 4, 'minecraft:potato', 'mysticalagriculture:tertium_farmland', 'crop', 'minecraft:potatoes')
+    gardenvanilla('minecraft:potato', 5, 'minecraft:potato', 'mysticalagriculture:imperium_farmland', 'crop', 'minecraft:potatoes')
+    gardenvanilla('minecraft:potato', 6, 'minecraft:potato', 'mysticalagriculture:supremium_farmland', 'crop', 'minecraft:potatoes')
+    gardenvanilla('minecraft:potato', 7, 'minecraft:potato', 'mysticalagradditions:insanium_farmland', 'crop', 'minecraft:potatoes')
+    
+    event.remove({ id: 'immersiveengineering:cloche/pumpkin' })
+    gardenvanilla('minecraft:pumpkin', 1, 'minecraft:pumpkin_seeds', 'minecraft:dirt', 'stem', 'minecraft:pumpkin')
+    gardenvanilla('minecraft:pumpkin', 2, 'minecraft:pumpkin_seeds', 'mysticalagriculture:inferium_farmland', 'stem', 'minecraft:pumpkin')
+    gardenvanilla('minecraft:pumpkin', 3, 'minecraft:pumpkin_seeds', 'mysticalagriculture:prudentium_farmland', 'stem', 'minecraft:pumpkin')
+    gardenvanilla('minecraft:pumpkin', 4, 'minecraft:pumpkin_seeds', 'mysticalagriculture:tertium_farmland', 'stem', 'minecraft:pumpkin')
+    gardenvanilla('minecraft:pumpkin', 5, 'minecraft:pumpkin_seeds', 'mysticalagriculture:imperium_farmland', 'stem', 'minecraft:pumpkin')
+    gardenvanilla('minecraft:pumpkin', 6, 'minecraft:pumpkin_seeds', 'mysticalagriculture:supremium_farmland', 'stem', 'minecraft:pumpkin')
+    gardenvanilla('minecraft:pumpkin', 7, 'minecraft:pumpkin_seeds', 'mysticalagradditions:insanium_farmland', 'stem', 'minecraft:pumpkin')
+    
+    event.remove({ id: 'immersiveengineering:cloche/melon' })
+    gardenvanilla('minecraft:melon', 1, 'minecraft:melon_seeds', 'minecraft:dirt', 'stem', 'minecraft:melon')
+    gardenvanilla('minecraft:melon', 2, 'minecraft:melon_seeds', 'mysticalagriculture:inferium_farmland', 'stem', 'minecraft:melon')
+    gardenvanilla('minecraft:melon', 3, 'minecraft:melon_seeds', 'mysticalagriculture:prudentium_farmland', 'stem', 'minecraft:melon')
+    gardenvanilla('minecraft:melon', 4, 'minecraft:melon_seeds', 'mysticalagriculture:tertium_farmland', 'stem', 'minecraft:melon')
+    gardenvanilla('minecraft:melon', 5, 'minecraft:melon_seeds', 'mysticalagriculture:imperium_farmland', 'stem', 'minecraft:melon')
+    gardenvanilla('minecraft:melon', 6, 'minecraft:melon_seeds', 'mysticalagriculture:supremium_farmland', 'stem', 'minecraft:melon')
+    gardenvanilla('minecraft:melon', 7, 'minecraft:melon_seeds', 'mysticalagradditions:insanium_farmland', 'stem', 'minecraft:melon')
+    
+    event.remove({ id: 'immersiveengineering:cloche/chorus_fruit' })
+    gardenvanilla('minecraft:chorus_fruit', 1, 'minecraft:chorus_flower', 'minecraft:end_stone', 'chorus', 'minecraft:chorus_flower')
+    
+    event.remove({ id: 'immersiveengineering:cloche/hemp' })
+    gardenvanilla('immersiveengineering:hemp_fiber', 1, 'immersiveengineering:seed', 'minecraft:dirt', 'hemp', 'immersiveengineering:hemp')
+    gardenvanilla('immersiveengineering:hemp_fiber', 2, 'immersiveengineering:seed', 'mysticalagriculture:inferium_farmland', 'hemp', 'immersiveengineering:hemp')
+    gardenvanilla('immersiveengineering:hemp_fiber', 3, 'immersiveengineering:seed', 'mysticalagriculture:prudentium_farmland', 'hemp', 'immersiveengineering:hemp')
+    gardenvanilla('immersiveengineering:hemp_fiber', 4, 'immersiveengineering:seed', 'mysticalagriculture:tertium_farmland', 'hemp', 'immersiveengineering:hemp')
+    gardenvanilla('immersiveengineering:hemp_fiber', 5, 'immersiveengineering:seed', 'mysticalagriculture:imperium_farmland', 'hemp', 'immersiveengineering:hemp')
+    gardenvanilla('immersiveengineering:hemp_fiber', 6, 'immersiveengineering:seed', 'mysticalagriculture:supremium_farmland', 'hemp', 'immersiveengineering:hemp')
+    gardenvanilla('immersiveengineering:hemp_fiber', 7, 'immersiveengineering:seed', 'mysticalagradditions:insanium_farmland', 'hemp', 'immersiveengineering:hemp')
+    
+    event.remove({ id: 'immersiveengineering:cloche/nether_warp' })
+    gardenvanilla('minecraft:nether_wart', 1, 'minecraft:nether_wart', 'minecraft:soul_sand', 'crop', 'minecraft:nether_wart')
+
+    gardenvanilla('byg:oddity_cactus', 1, 'byg:oddity_cactus', 'byg:end_sand', 'stacking', 'byg:oddity_cactus')
+
+    function gardenvanilla(result, qte, input, soil, type, block){event.custom({"type":"immersiveengineering:cloche","results":[{"item":result,"count":qte}],"input":{"item":input},"soil":{"item" : soil},"time":50,"render":{"type":type,"block": block}})}
+
+    //Quark Crystals
+    gardenvanilla('quark:red_crystal_cluster', 8, 'quark:red_crystal_cluster', 'quark:red_crystal', 'stacking', 'quark:red_crystal')
+    gardenvanilla('quark:orange_crystal_cluster', 8, 'quark:orange_crystal_cluster', 'quark:orange_crystal', 'stacking', 'quark:orange_crystal')
+    gardenvanilla('quark:yellow_crystal_cluster', 8, 'quark:yellow_crystal_cluster', 'quark:yellow_crystal', 'stacking', 'quark:yellow_crystal')
+    gardenvanilla('quark:green_crystal_cluster', 8, 'quark:green_crystal_cluster', 'quark:green_crystal', 'stacking', 'quark:green_crystal')
+    gardenvanilla('quark:blue_crystal_cluster', 8, 'quark:blue_crystal_cluster', 'quark:blue_crystal', 'stacking', 'quark:blue_crystal')
+    gardenvanilla('quark:indigo_crystal_cluster', 8, 'quark:indigo_crystal_cluster', 'quark:indigo_crystal', 'stacking', 'quark:indigo_crystal')
+    gardenvanilla('quark:violet_crystal_cluster', 8, 'quark:violet_crystal_cluster', 'quark:violet_crystal', 'stacking', 'quark:violet_crystal')
+    gardenvanilla('quark:white_crystal_cluster', 8, 'quark:white_crystal_cluster', 'quark:white_crystal', 'stacking', 'quark:white_crystal')
+    gardenvanilla('quark:black_crystal_cluster', 8, 'quark:black_crystal_cluster', 'quark:black_crystal', 'stacking', 'quark:black_crystal')
+
+    /*********************************** MYSTICAL AGRICULTURE ***********************************/
+    function gardenma(crop, soil, qte){event.custom({"type": "immersiveengineering:cloche", "input": { "item": "mysticalagriculture:"+crop+"_seeds" }, "soil": { "item": soil }, "time": 50, "render": { "type": "crop", "block": "mysticalagriculture:"+crop+"_crop" }, "results": [{ "item": "mysticalagriculture:"+crop+"_essence", "count": qte },{"item":"mysticalagriculture:fertilized_essence","count":1}]})}
+    var crop = ["air", "earth", "water", "fire", "inferium", "stone", "dirt", "wood", "ice", "deepslate", "nature", "dye", "nether", "coal", "coral", "honey", "amethyst", "pig", "chicken", "cow", "sheep", "squid", "fish", "slime", "turtle", "rubber", "silicon", "sulfur", "copper", "mystical_flower", "apatite", "iron", "aluminum", "salpeter", "nether_quartz", "glowstone", "redstone", "obsidian", "prismarine", "zombie", "skeleton", "creeper", "spider", "rabbit", "tin", "bronze", "zinc", "brass", "silver", "lead", "graphite", "blizz", "blitz", "basalz", "amethyst_bronze", "manasteel", "ironwood", "steeleaf", "rose_gold", "slimesteel", "pig_iron", "quartz_enriched_iron", "gold", "lapis_lazuli", "end", "experience", "blaze", "ghast", "enderman", "steel", "nickel", "constantan", "electrum", "invar", "tungsten", "uranium", "signalum", "lumium", "hop_graphite", "elementium", "osmium", "refined_glowstone", "refined_obsidian", "fluorite", "fiery_ingot", "knightmetal", "cobalt", "hepatizon", "sapphire", "ruby","diamond", "emerald", "netherite", "wither_skeleton", "platinum", "enderium", "terrasteel", "yellorium", "cyanite", "manyullyn", "queens_slime", "iridium", "fluix", "chrome", "titanium", "mithril", "certus_quartz", "sky_stone", "limestone", "saltpeter"]
+    crop.forEach(C => { gardenma(C, "minecraft:dirt", 1) })
+    crop.forEach(C => { gardenma(C, "mysticalagriculture:inferium_farmland", 2) })
+    crop.forEach(C => { gardenma(C, "mysticalagriculture:prudentium_farmland", 3) })
+    crop.forEach(C => { gardenma(C, "mysticalagriculture:tertium_farmland", 4) })
+    crop.forEach(C => { gardenma(C, "mysticalagriculture:imperium_farmland", 5) })
+    crop.forEach(C => { gardenma(C, "mysticalagriculture:supremium_farmland", 6) })
+    crop.forEach(C => { gardenma(C, "mysticalagradditions:insanium_farmland", 7) })
+
+    //Nether Star
+    event.custom({
+        "type": "immersiveengineering:cloche", "input": { "item": "mysticalagriculture:nether_star_seeds" }, "soil": { "item": "mysticalagradditions:nether_star_crux" }, "time": 50, "render": { "type": "crop", "block": "mysticalagriculture:nether_star_crop" }, "results": [{ "item": "mysticalagriculture:nether_star_essence", "count": 8 },{"item":"mysticalagriculture:fertilized_essence","count":1}]
+    })
+    //Dragon Egg
+    event.custom({
+        "type": "immersiveengineering:cloche", "input": { "item": "mysticalagriculture:dragon_egg_seeds" }, "soil": { "item": "mysticalagradditions:dragon_egg_crux" }, "time": 50, "render": { "type": "crop", "block": "mysticalagriculture:dragon_egg_crop" }, "results": [{ "item": "mysticalagriculture:dragon_egg_essence", "count": 8 },{"item":"mysticalagriculture:fertilized_essence","count":1}]
+    })
+
+})
